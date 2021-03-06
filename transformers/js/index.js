c7bbf3e716f2bb5c45b009923e2855fa8d9add3a
Simon Chervenak <sarras305@gmail.com>
2021-03-06 17:35:50 -0500

    working on scrolling

--- transformers/js/index.js
+++ transformers/js/index.js
@@ -1,3 +1,13 @@
 $("#extract").click(() => {
     $(".flip-card-inner").toggleClass("flipped");
 });
+
+// let t = new Tether({
+//     element: $("#element"),
+//     target: $("#tether"),
+//     attachment: 'top right',
+//     targetAttachment: 'top left',
+//     // offset: '0 10px',
+//     // targetOffset: '20px 0'
+// });
+//

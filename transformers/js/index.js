fcc1b20afbb1a21c4177a048ab6181019595b0f3
Simon Chervenak <sarras305@gmail.com>
2021-03-06 16:43:53 -0500

    add flip thingy

--- transformers/js/index.js
+++ transformers/js/index.js
@@ -1,17 +1,3 @@
-// let flip = 0;
-//
-// let flipfn = () => {
-//     console.log(flip);
-//     if (flip === 0) {
-//         flip = 1;
-//         $(".flip-card-back").show();
-//         $(".flip-card-front").hide();
-//     } else {
-//         flip = 0;
-//         $(".flip-card-back").hide();
-//         $(".flip-card-front").show();
-//     }
-// }
-//
-// $(".flip-card").mouseenter(flipfn);
-// $(".flip-card").mouseleave(flipfn);
+$("#extract").click(() => {
+    $(".flip-card-inner").toggleClass("flipped");
+});

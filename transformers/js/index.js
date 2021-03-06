cf0372be754c175e4eae5a2863af175004aa6aeb
Simon Chervenak <sarras305@gmail.com>
2021-03-06 16:22:53 -0500

    add flip animation

---
+++ transformers/js/index.js
@@ -0,0 +1,17 @@
+// let flip = 0;
+//
+// let flipfn = () => {
+//     console.log(flip);
+//     if (flip === 0) {
+//         flip = 1;
+//         $(".flip-card-back").show();
+//         $(".flip-card-front").hide();
+//     } else {
+//         flip = 0;
+//         $(".flip-card-back").hide();
+//         $(".flip-card-front").show();
+//     }
+// }
+//
+// $(".flip-card").mouseenter(flipfn);
+// $(".flip-card").mouseleave(flipfn);

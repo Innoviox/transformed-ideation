bac89c17e2e6a32dee2419e7507dd342208df6b8
Simon Chervenak <sarras305@gmail.com>
2021-03-06 19:22:36 -0500

    implement add

--- transformers/js/index.js
+++ transformers/js/index.js
@@ -11,6 +11,40 @@ let modal = () => {
 
 $("#export, .closemodal").click(modal);
 
-$(".delete-flashcard").click((e) => {
+let del = () => $(".delete-flashcard").click((e) => {
     e.target.parentElement.remove();
 });
+
+let add = () => {
+    document.getElementById('flashcards').insertAdjacentHTML('beforeend', '<div class="flashcard notification">\n' +
+        '                                <button class="delete delete-flashcard"></button>\n' +
+        '                                <div class="field is-horizontal">\n' +
+        '                                    <div class="field-label is-normal">\n' +
+        '                                        <label class="label">Front</label>\n' +
+        '                                    </div>\n' +
+        '                                    <div class="field-body">\n' +
+        '                                        <div class="field">\n' +
+        '                                            <p class="control">\n' +
+        '                                                <input class="input" type="text" placeholder="Text">\n' +
+        '                                            </p>\n' +
+        '                                        </div>\n' +
+        '                                    </div>\n' +
+        '                                </div>\n' +
+        '                                <div class="field is-horizontal">\n' +
+        '                                    <div class="field-label is-normal">\n' +
+        '                                        <label class="label">Back</label>\n' +
+        '                                    </div>\n' +
+        '                                    <div class="field-body">\n' +
+        '                                        <div class="field">\n' +
+        '                                            <p class="control">\n' +
+        '                                                <input class="input" type="text" placeholder="Text">\n' +
+        '                                            </p>\n' +
+        '                                        </div>\n' +
+        '                                    </div>\n' +
+        '                                </div>\n' +
+        '                            </div>');
+    del();
+};
+
+$("#add").click(add);
+$(document).ready(add);

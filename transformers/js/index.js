eae96b741408f2901feda865636d050f9fce6f9e
Simon Chervenak <sarras305@gmail.com>
2021-03-06 19:16:38 -0500

    remove libs

--- transformers/js/index.js
+++ transformers/js/index.js
@@ -14,13 +14,3 @@ $("#export, .closemodal").click(modal);
 $(".delete-flashcard").click((e) => {
     e.target.parentElement.remove();
 });
-
-// let t = new Tether({
-//     element: $("#element"),
-//     target: $("#tether"),
-//     attachment: 'top right',
-//     targetAttachment: 'top left',
-//     // offset: '0 10px',
-//     // targetOffset: '20px 0'
-// });
-//
